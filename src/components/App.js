1201c8ec67cad50f6799056a4c2c90fc5a7ee912
abhilaad <71024710+abhilaad@users.noreply.github.com>
2021-08-12 00:07:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,31 @@ class App extends Component {
     constructor(props) {
 		super(props);
 	    this.state = { show : false };
+      this.updateState = this.updateState.bind(this) 
+      
 	};
+  updateState() { 
+      
+    this.setState({show: !(this.state.show)}) 
+  } 
 
     render() {
     	return(
     		<div id="main">
 			<button
         id="click"
-        onClick={function () {
-          this.setState({show: !show});
-        }}
+        onClick={
+          this.updateState
+        }
       >
         Toggle P
       </button>
-      {show ? (
+      {this.state.show ? (
         <p id="para">
           Hello, I've learnt to use the full-stack evaluation tool. This makes
           me so happy
         </p>
-      ) : null}
+      ) : null }
     		</div>
     	);
     }
